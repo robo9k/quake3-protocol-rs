f08fa4738c0ddda7b13bb11f51643b10bd7e59f7
robot9001 <robo9k@symlink.io>
2024-04-20 21:11:06 +0200

    implement packet sequence

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -1,5 +1,41 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use quake3::net::chan::FRAGMENT_BIT;
+use std::ffi::c_int;
+
+#[repr(transparent)]
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+pub struct PacketSequenceNumber(c_int);
+
+impl PacketSequenceNumber {
+    // this should probably fail if -1 or FRAGMENT_BIT
+    pub fn new(bits: c_int) -> Self {
+        Self(bits)
+    }
+}
+
+#[repr(transparent)]
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+pub struct PacketSequence(c_int);
+
+impl PacketSequence {
+    pub fn new(bits: c_int) -> Self {
+        Self(bits)
+    }
+
+    pub fn new_with_number_and_fragment(number: PacketSequenceNumber, fragmented: bool) -> Self {
+        if fragmented {
+            Self(number.0 | FRAGMENT_BIT)
+        } else {
+            Self(number.0 & !FRAGMENT_BIT)
+        }
+    }
+
+    pub fn is_fragmented(&self) -> bool {
+        self.0 & FRAGMENT_BIT != 0
+    }
+
+    pub fn number(&self) -> PacketSequenceNumber {
+        PacketSequenceNumber(self.0 & !FRAGMENT_BIT)
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +43,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn packetsequence_new_with_number_and_fragment() {
+        let sequence =
+            PacketSequence::new_with_number_and_fragment(PacketSequenceNumber::new(42), true);
+        assert!(sequence.is_fragmented());
+        assert_eq!(sequence.number(), PacketSequenceNumber::new(42));
+
+        let sequence =
+            PacketSequence::new_with_number_and_fragment(PacketSequenceNumber::new(69), false);
+        assert!(!sequence.is_fragmented());
+        assert_eq!(sequence.number(), PacketSequenceNumber::new(69));
     }
 }
---
+++ quake3/src/net/chan.rs
@@ -0,0 +1,3 @@
+use std::ffi::c_int;
+
+pub const FRAGMENT_BIT: c_int = 1 << 31;
